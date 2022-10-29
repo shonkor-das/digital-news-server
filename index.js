bc3d9e21b7829bd6178726fac629073d5b48d812
Shonkor Das <94380595+shonkor-das@users.noreply.github.com>
2022-10-29 15:14:56 +0600

    server added

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+const categories = require('./Data/Categories.json');
+
+app.get('/',(req, res) =>{
+    res.send('News API Running');
+});
+
+app.get('/news-categories',(req, res) =>{
+    res.send(categories);
+});
+
+app.listen(port, () =>{
+    console.log('Digital News Server Running on Port', port);
+})
